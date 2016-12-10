e014861d52095539a95eaffd6fa4ea1fb318af4b
Andrei-Dragos Dobrescu <andrei-dragos.dobrescu@student.manchester.ac.uk>
2016-12-10 19:00:14 +0000

    Added seed distribution method for simulating moves.

--- MKAgent/Main.java
+++ MKAgent/Main.java
@@ -6,6 +6,58 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 
 /**
+ * Method that handles the seed distribution once a move is simulated.
+ */
+public void distSeeds (int seedNum, Side side, int holeNum, int noOfHoles, Board board, Side playerSide, Node currentNode)
+{
+        int index = holeNumm + 1;
+        while (seedNum != 0) && (index <= noOfHoles)
+        {
+                seedNum--;
+                board.setSeeds(side, index, board.getSeeds(side, index) + 1);
+                index++;
+        }//while
+
+        if (seeds != 0)
+        {
+                if (side == playerSide)
+                        currentNode.gain++;
+                else
+                        currentNode.gain--;
+
+                seedNum--;
+                distSeeds(seedNum, side.opposite(), 0, noOfHoles, board, playerSide, currentNode);
+        }//if
+
+        else
+	{
+                if (side == playerSide) && (board.getSeeds(side, index) == 1)
+                {
+                        currentNode.gain += board.getSeedsOp(side, index);
+                        board.setSeedsOp(side, index, 0);
+                }//if
+		boolean noEnd = false;
+
+		for(int i = 1; i<= noOfHoles; i++)
+		{
+			if (board.getSeeds(playerSide, i) != 0)
+			{
+				noEnd = true;
+				i = noOfHoles + 1;
+			}//if
+		}//for
+
+		if (!noEnd)
+			for(int i = 1; i<= noOfHoles; i++)
+			{
+				currentNode.gain -= board.getSeeds(playerSide.opposite, i);
+				board.setSeeds(playerSide.opposite, i, 0);
+			}//for
+	}//else	
+
+}//distSeeds
+
+/**
  * The main application class. It also provides methods for communication
  * with the game engine.
  */
@@ -57,6 +109,7 @@ public class Main
     	try {
 			String s, msj;
 			boolean first = true;
+			Side mySide = NORTH;
 			Board b = new Board(7,7);
 			while (true){
 				System.err.println();
@@ -73,6 +126,7 @@ public class Main
 							if (first){
 								// msj = Protocol.createMoveMsg(1);
 								// sendMsg(msj);
+								mySide = SOUTH;
 								msj = Protocol.createMoveMsg(b.getNoOfHoles());
 								sendMsg(msj);
 							} // if
@@ -85,9 +139,11 @@ public class Main
 							// @SwapMove
 							if (!first && r.move == b.getNoOfHoles()) {
 								first = true;
+								mySide = SOUTH;
 								msj = Protocol.createSwapMsg();
 								sendMsg(msj);
 							} // if
+							
 
 							System.err.println("A state.");
 							System.err.println("This was the move: " + r.move);
