dc042568120a64ff56acbc39aa444febd7020729
woemike <ionmihai09@gmail.com>
2016-12-11 16:52:09 +0000

    south and north

--- MKAgent/Main.java
+++ MKAgent/Main.java
@@ -8,54 +8,54 @@ import java.io.Reader;
 /**
  * Method that handles the seed distribution once a move is simulated.
  */
-public void distSeeds (int seedNum, Side side, int holeNum, int noOfHoles, Board board, Side playerSide, Node currentNode)
-{
-        int index = holeNumm + 1;
-        while (seedNum != 0) && (index <= noOfHoles)
-        {
-                seedNum--;
-                board.setSeeds(side, index, board.getSeeds(side, index) + 1);
-                index++;
-        }//while
+// public void distSeeds (int seedNum, Side side, int holeNum, int noOfHoles, Board board, Side playerSide, Node currentNode)
+// {
+//         int index = holeNumm + 1;
+//         while (seedNum != 0) && (index <= noOfHoles)
+//         {
+//                 seedNum--;
+//                 board.setSeeds(side, index, board.getSeeds(side, index) + 1);
+//                 index++;
+//         }//while
 
-        if (seeds != 0)
-        {
-                if (side == playerSide)
-                        currentNode.gain++;
-                else
-                        currentNode.gain--;
+//         if (seeds != 0)
+//         {
+//                 if (side == playerSide)
+//                         currentNode.gain++;
+//                 else
+//                         currentNode.gain--;
 
-                seedNum--;
-                distSeeds(seedNum, side.opposite(), 0, noOfHoles, board, playerSide, currentNode);
-        }//if
+//                 seedNum--;
+//                 distSeeds(seedNum, side.opposite(), 0, noOfHoles, board, playerSide, currentNode);
+//         }//if
 
-        else
-	{
-                if (side == playerSide) && (board.getSeeds(side, index) == 1)
-                {
-                        currentNode.gain += board.getSeedsOp(side, index);
-                        board.setSeedsOp(side, index, 0);
-                }//if
-		boolean noEnd = false;
+//         else
+// 	{
+//                 if (side == playerSide) && (board.getSeeds(side, index) == 1)
+//                 {
+//                         currentNode.gain += board.getSeedsOp(side, index);
+//                         board.setSeedsOp(side, index, 0);
+//                 }//if
+// 		boolean noEnd = false;
 
-		for(int i = 1; i<= noOfHoles; i++)
-		{
-			if (board.getSeeds(playerSide, i) != 0)
-			{
-				noEnd = true;
-				i = noOfHoles + 1;
-			}//if
-		}//for
+// 		for(int i = 1; i<= noOfHoles; i++)
+// 		{
+// 			if (board.getSeeds(playerSide, i) != 0)
+// 			{
+// 				noEnd = true;
+// 				i = noOfHoles + 1;
+// 			}//if
+// 		}//for
 
-		if (!noEnd)
-			for(int i = 1; i<= noOfHoles; i++)
-			{
-				currentNode.gain -= board.getSeeds(playerSide.opposite, i);
-				board.setSeeds(playerSide.opposite, i, 0);
-			}//for
-	}//else	
+// 		if (!noEnd)
+// 			for(int i = 1; i<= noOfHoles; i++)
+// 			{
+// 				currentNode.gain -= board.getSeeds(playerSide.opposite, i);
+// 				board.setSeeds(playerSide.opposite, i, 0);
+// 			}//for
+// 	}//else	
 
-}//distSeeds
+// }//distSeeds
 
 /**
  * The main application class. It also provides methods for communication
@@ -109,7 +109,11 @@ public class Main
     	try {
 			String s, msj;
 			boolean first = true;
-			Side mySide = NORTH;
+
+			Side north = Side.values()[0];
+			Side south = Side.values()[1];
+			Side mySide = north;
+
 			Board b = new Board(7,7);
 			while (true){
 				System.err.println();
@@ -124,7 +128,7 @@ public class Main
 
 							// @SuperMove
 							if (first){
-								mySide = SOUTH;
+								mySide = south;
 								msj = Protocol.createMoveMsg(b.getNoOfHoles());
 								sendMsg(msj);
 							} // if
@@ -136,13 +140,13 @@ public class Main
 
 							// If opponent swaps
 							if (r.move == -1){
-								mySide = NORTH;
+								mySide = north;
 							}
 
 							// @SwapMove
 							if (!first && r.move == b.getNoOfHoles()) {
 								first = true;
-								mySide = SOUTH;
+								mySide = south;
 								msj = Protocol.createSwapMsg();
 								sendMsg(msj);
 							} // if
