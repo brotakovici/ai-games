ab2c2de5303681b50ce4b005fe440d2ee3b734c7
woemike <ionmihai09@gmail.com>
2016-12-15 16:59:48 +0000

    22

--- MKAgent/Main.java
+++ MKAgent/Main.java
@@ -28,17 +28,28 @@ public class Main
     	System.out.flush();
     }
 
-    public static Node updateNode (int move, Node rootNode){
-        ArrayList<Node> possibleNextNodes = rootNode.getChildren();
-        Node nextNode = null;
-        for(Node node : possibleNextNodes){
-            if(node.getMoveMade().getHole() == move){
-                nextNode = node;
+    public static int mvMk (Board b, Move move, Kalah kal, Side mySide) {
+        int i = 0;
+        for (i = 1; i <= 7; i++){
+            if (b.getSeeds(mySide, i) != 0){
+                move = new Move(mySide, i);
+                kal.makeMove(b, move);
+                b = kal.getBoard();
                 break;
             }
         }
-        rootNode = nextNode;
-        rootNode.generateLevel(DEPTH);
+
+        return i;
+    }
+
+    public static Node updateNode (int move, Node rootNode){
+        ArrayList<Node> asda = rootNode.getChildren();
+        for(Node node : asda) {
+            if(node.getMoveMade().getHole() == move){
+                rootNode = node;
+                rootNode.generateLevel(DEPTH);
+            }
+        }
 
         return rootNode;
 
@@ -74,6 +85,7 @@ public class Main
 	{
     	try {
 			String s, msj;
+            int mv = 0;
 			boolean first = true, swap = false;
 
 			Side north = Side.values()[0];
@@ -81,10 +93,10 @@ public class Main
 			Side mySide = north; 
 
 			Board b = new Board(7,7);
-                                    Kalah kal = new Kalah(b);
-                                    Move move = null;
+            Kalah kal = new Kalah(b);
+            Move move = null;
 
-                                    Node rootNode = null;
+            Node rootNode = null;
 
 			while (true){
 				System.err.println();
@@ -99,13 +111,13 @@ public class Main
 
 							// SuperMove
 							if (first && !swap){
-                                                                                                swap = true;
+                                swap = true;
 								mySide = south;
 								msj = Protocol.createMoveMsg(b.getNoOfHoles());
 								sendMsg(msj);
-                                                                                                move = new Move(south, b.getNoOfHoles());
-                                                                                                kal.makeMove(b, move);
-                                                                                                b = kal.getBoard();
+                                move = new Move(south, b.getNoOfHoles());
+                                kal.makeMove(b, move);
+                                b = kal.getBoard();
 							} // if
 
 							System.err.println("Starting player? " + first);
@@ -115,7 +127,7 @@ public class Main
 
 							// // If opponent swaps
 							// if (r.move == -1){
-       //                                                                                          swap = true;
+                            //  swap = true;
 							// 	mySide = north;
 							// }
 
@@ -126,40 +138,44 @@ public class Main
 							// 	msj = Protocol.createSwapMsg();
 							// 	sendMsg(msj);
 							// } // if
+                            if (!r.end){
+                                if (r.again && rootNode == null){
+                                    rootNode = new Node(b, mySide, mySide);
+                                    rootNode.generateChildren(DEPTH);
+                                } else if (!r.again && rootNode == null)
+                                    if (mySide == north){
+                                        rootNode = new Node(b, south, mySide);
+                                        rootNode.generateChildren(DEPTH);
+                                    } else {
+                                        rootNode = new Node(b, north, mySide);
+                                        rootNode.generateChildren(DEPTH);
+                                    }
 
-                                                                                    // Opponent's move
-                                                                                    if (!first){
-                                                                                        mySide = north;
-                                                                                        move = new Move(south, r.move);
-                                                                                        kal.makeMove(b, move);
-                                                                                        b = kal.getBoard();
-                                                                                    } // if
+                                if (!r.again){
+                                    move = new Move(south, r.move);
+                                    kal.makeMove(b, move);
+                                    b = kal.getBoard();
 
-                                                                                    if (rootNode == null && first) 
-                                                                                        rootNode = new Node(b, north, mySide);
-                                                                                    else if (rootNode == null && !first && !swap)
-                                                                                        rootNode = new Node(b, south, mySide);
-                                                                                    else if (rootNode == null && !first && swap)
-                                                                                        rootNode = new Node(b, north, mySide);
+                                    rootNode = updateNode(r.move, rootNode);
+                                }
 
-                                                                                    if (rootNode.getChildren().isEmpty())
-                                                                                        rootNode.generateChildren(DEPTH);
-                                                                                    if (!r.end){
-                                                                                        if (r.again){
-                                                                                            Move pizdaMasii = rootNode.getMoveToMake();
-                                                                                            msj = Protocol.createMoveMsg(pizdaMasii.getHole());
-                                                                                            sendMsg(msj);
-                                                                                            rootNode = updateNode(pizdaMasii.getHole(), rootNode);
-                                                                                        } else if(!r.again) { 
-                                                                                        //     rootNode = updateNode(r.move, rootNode);
-                                                                                        }
-                                                                                    }
-                                                                                    
+                                if(r.again){
+                                    move = rootNode.getMoveToMake();
+                                    kal.makeMove(b, move);
+                                    b = kal.getBoard();
+
+                                    //mv = mvMk(b, move, kal, mySide);
+                                    
+                                    rootNode = updateNode(move.getHole(), rootNode);
+
+                                    msj = Protocol.createMoveMsg(move.getHole());
+                                    sendMsg(msj);
+                                }
+                            }               
+
 							System.err.println("A state.");
 							System.err.println("This was the move: " + r.move);
 							System.err.println("Is the game over? " + r.end);
-							if (!r.end) System.err.println("Is it our turn again? " + r.again);
-							System.err.print("The board:\n" + b);
 							break; // State
 						case END: 
 							System.err.println("An end. Bye bye!"); 
